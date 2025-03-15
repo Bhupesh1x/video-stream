3e946ecee1e443619cb5cf7106e4fc5e875fbf65
Bhupesh <bhupeshvyas202@gmail.com>
2025-03-15 18:08:03 +0530

    tRPC initial setup in the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
+import { TRPCProvider } from "@/trpc/client";
+
 import "./globals.css";
 
 const font = Inter({
@@ -21,7 +23,9 @@ export default function RootLayout({
   return (
     <ClerkProvider afterSignOutUrl="/">
       <html lang="en">
-        <body className={font.className}>{children}</body>
+        <body className={font.className}>
+          <TRPCProvider>{children}</TRPCProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
