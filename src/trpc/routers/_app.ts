6194aa394068d20e23a3b32f4511ef341e008b2b
Bhupesh <bhupeshvyas202@gmail.com>
2025-03-16 17:46:37 +0530

    Created procedure for categories and created get all categories call

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,19 +1,9 @@
-import { z } from "zod";
+import { categoriesRouter } from "@/features/categories/server/procedures";
 
-import { baseProcedure, createTRPCRouter } from "../init";
+import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
+  categories: categoriesRouter,
 });
 
 // export type definition of API
