217f1f979f200db089ab03570be338b49c66ca6e
Bhupesh <bhupeshvyas202@gmail.com>
2025-03-22 19:25:04 +0530

    Created reusable component and hook for infinite scroll

---
+++ src/components/InfiniteScroll.tsx
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
+
+import { Button } from "./ui/button";
+
+type Props = {
+  isManual?: boolean;
+  hasNextPage: boolean;
+  isFetchingNextPage: boolean;
+  fetchNextPage: () => void;
+};
+
+export function InfiniteScroll({
+  isManual = false,
+  hasNextPage,
+  isFetchingNextPage,
+  fetchNextPage,
+}: Props) {
+  const { targetRef, isIntersecting } = useIntersectionObserver();
+
+  useEffect(() => {
+    if (hasNextPage && isIntersecting && !isFetchingNextPage && !isManual) {
+      fetchNextPage();
+    }
+  }, [
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isIntersecting,
+    isManual,
+  ]);
+
+  return (
+    <div className="flex flex-col items-center p-4 gap-4">
+      <div ref={targetRef} className="h-1" />
+
+      {hasNextPage ? (
+        <Button
+          variant="secondary"
+          disabled={!hasNextPage && isFetchingNextPage}
+          onClick={fetchNextPage}
+        >
+          {isFetchingNextPage ? "Loading..." : "Load more"}
+        </Button>
+      ) : (
+        <p className="text-xs text-muted-foreground">
+          You have reached the end of the list
+        </p>
+      )}
+    </div>
+  );
+}
