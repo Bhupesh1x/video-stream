8085cebfaa561f146dc6d48af4be6696fde1d736
Bhupesh <bhupeshvyas202@gmail.com>
2025-03-23 16:54:07 +0530

    Created responsive modal component using dialog and drawer

---
+++ src/components/ResponsiveModal.tsx
@@ -0,0 +1,44 @@
+import { useIsMobile } from "@/hooks/use-mobile";
+
+import { Drawer, DrawerContent, DrawerHeader, DrawerTitle } from "./ui/drawer";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+
+type Props = {
+  title: string;
+  children: React.ReactNode;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+};
+
+export function ResponsiveModal({
+  open,
+  title,
+  children,
+  onOpenChange,
+}: Props) {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <Drawer open={open} onOpenChange={onOpenChange}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{title}</DrawerTitle>
+          </DrawerHeader>
+          {children}
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
+  return (
+    <Dialog>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+}
