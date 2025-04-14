f1ec621520bdf060bb33360e203f09ac4adb9c2b
Bhupesh <bhupeshvyas202@gmail.com>
2025-04-14 17:13:30 +0530

    created create and get all comments procedures

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,7 @@
 import { videoRouter } from "@/features/videos/server/procedures";
 import { studioRouter } from "@/features/studio/server/procedures";
 import { videoViewsRouter } from "@/features/videoViews/procedures";
+import { commentsRouter } from "@/features/comments/server/procedures";
 import { categoriesRouter } from "@/features/categories/server/procedures";
 import { videoReactionsRouter } from "@/features/videoReactions/procedures";
 import { subscriptionsRouter } from "@/features/subscriptions/server/procedures";
@@ -10,6 +11,7 @@ import { createTRPCRouter } from "../init";
 export const appRouter = createTRPCRouter({
   video: videoRouter,
   studio: studioRouter,
+  comments: commentsRouter,
   categories: categoriesRouter,
   videoViews: videoViewsRouter,
   subscriptions: subscriptionsRouter,
