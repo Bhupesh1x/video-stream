1672a9842e58b63f4976a359064b02926f9258e2
Bhupesh <bhupeshvyas202@gmail.com>
2025-04-18 20:18:21 +0530

    Updated getMany comments-procedure for pagination

--- src/features/comments/server/procedures.ts
+++ src/features/comments/server/procedures.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { eq, getTableColumns } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
 
 import {
   baseProcedure,
@@ -37,20 +37,78 @@ export const commentsRouter = createTRPCRouter({
     .input(
       z.object({
         videoId: z.string().uuid(),
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
       })
     )
     .query(async ({ input }) => {
-      const { videoId } = input;
+      const { videoId, limit, cursor } = input;
 
-      const data = await db
+      const totalCountPromise = db
+        .select({ count: count() })
+        .from(comments)
+        .where(eq(comments.videoId, videoId));
+
+      const dataPromise = db
         .select({
           ...getTableColumns(comments),
           user: users,
         })
         .from(comments)
-        .where(eq(comments.videoId, videoId))
-        .innerJoin(users, eq(comments.userId, users.id));
+        .where(
+          and(
+            eq(comments.videoId, videoId),
+            cursor
+              ? or(
+                  lt(comments.updatedAt, cursor.updatedAt),
+                  and(
+                    eq(comments.updatedAt, cursor.updatedAt),
+                    lt(comments.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .innerJoin(users, eq(comments.userId, users.id))
+        .orderBy(desc(comments.updatedAt), desc(comments.id))
+        .limit(limit + 1);
 
-      return data;
+      const [totalCountResult, dataResult] = await Promise.allSettled([
+        totalCountPromise,
+        dataPromise,
+      ]);
+
+      if (dataResult.status !== "fulfilled") {
+        throw new Error("Failed to fetch comments or count.");
+      }
+
+      let totalCount = { count: 0 };
+      if (totalCountResult.status === "fulfilled") {
+        totalCount = totalCountResult?.value?.[0] ?? { count: 0 };
+      }
+
+      const data = dataResult?.value ?? [];
+
+      const hasMoreData = data?.length > limit;
+      const items = hasMoreData ? data.slice(0, -1) : data;
+      const lastItem = items[items?.length - 1];
+
+      const nextCursor = hasMoreData
+        ? {
+            id: lastItem.id,
+            updatedAt: lastItem.updatedAt,
+          }
+        : null;
+
+      return {
+        totalCount: totalCount.count,
+        items,
+        nextCursor,
+      };
     }),
 });
