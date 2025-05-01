ade2699c63347d69dfaf25a83e84c880feae35fb
Bhupesh <bhupeshvyas202@gmail.com>
2025-05-02 00:30:57 +0530

    created procedure for get viewer liked videos

--- src/features/playlists/server/procedure.ts
+++ src/features/playlists/server/procedure.ts
@@ -94,4 +94,99 @@ export const playlistsRouter = createTRPCRouter({
         nextCursor,
       };
     }),
+  getLiked: protectedProcedure
+    .input(
+      z.object({
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            likedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const { limit, cursor } = input;
+      const { id: userId } = ctx.user;
+
+      const viewerVideoReaction = db.$with("viewer_video_reaction").as(
+        db
+          .select({
+            videoId: videoReactions.videoId,
+            likedAt: videoReactions.updatedAt,
+          })
+          .from(videoReactions)
+          .where(
+            and(
+              eq(videoReactions.userId, userId),
+              eq(videoReactions.type, "like")
+            )
+          )
+      );
+
+      const data = await db
+        .with(viewerVideoReaction)
+        .select({
+          ...getTableColumns(videos),
+          user: users,
+          likedAt: viewerVideoReaction.likedAt,
+          viewsCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+          likeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "like")
+            )
+          ),
+          dislikeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "dislike")
+            )
+          ),
+        })
+        .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
+        .innerJoin(
+          viewerVideoReaction,
+          eq(viewerVideoReaction.videoId, videos.id)
+        )
+        .where(
+          and(
+            eq(videos.visibility, "public"),
+            cursor
+              ? or(
+                  lt(viewerVideoReaction.likedAt, cursor.likedAt),
+                  and(
+                    eq(viewerVideoReaction.likedAt, cursor.likedAt),
+                    lt(videos.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .orderBy(desc(viewerVideoReaction.likedAt), desc(videos.id))
+        // Load 1 extra to check if there is more data
+        .limit(limit + 1);
+
+      const hasMoreData = data?.length > limit;
+
+      const items = hasMoreData ? data.slice(0, -1) : data;
+
+      const lastItem = items[items?.length - 1];
+
+      const nextCursor = hasMoreData
+        ? {
+            id: lastItem.id,
+            likedAt: lastItem.likedAt,
+          }
+        : null;
+
+      return {
+        items,
+        nextCursor,
+      };
+    }),
 });
