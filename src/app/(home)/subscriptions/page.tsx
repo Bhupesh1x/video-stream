d21c1f264957986d82f1cb1f9c19b0a77f04684e
Bhupesh <bhupeshvyas202@gmail.com>
2025-05-11 17:46:25 +0530

    Update page.tsx

--- src/app/(home)/subscriptions/page.tsx
+++ src/app/(home)/subscriptions/page.tsx
@@ -4,6 +4,8 @@ import { DEFAULT_LIMIT } from "@/constants";
 
 import { SubscriptionView } from "@/features/subscriptions/views/SubscriptionView";
 
+export const dynamic = "force-dynamic";
+
 function SubscriptionsPage() {
   void trpc.subscriptions.getMany.prefetchInfinite({
     limit: DEFAULT_LIMIT,
